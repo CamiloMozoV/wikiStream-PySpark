744404e3ce52994644a864e9a814776d09d845f6
Camilo Mozo <camilomozov@gmail.com>
2022-10-06 19:02:38 -0500

    refactoring code #001  Create a new function to performs transformation

--- sparkApps/src/kafkaSparkConsumer.py
+++ sparkApps/src/kafkaSparkConsumer.py
@@ -1,5 +1,11 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import (from_json, col)
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import (
+    from_json, 
+    col, 
+    from_unixtime, 
+    to_date, 
+    to_timestamp
+)
 from pyspark.sql.types import (
     StringType,
     StructField,
@@ -9,8 +15,8 @@ from pyspark.sql.types import (
     LongType
 )
 
-def readKafkaStream(spark: SparkSession) -> None:
-
+def read_kafka_stream(spark: SparkSession) -> DataFrame:
+    # Read the kafka topic [wikistream] 
     df_raw = spark.readStream\
                   .format("kafka")\
                   .option("kafka.bootstrap.servers", "spark-kafka:9092")\
@@ -20,9 +26,11 @@ def readKafkaStream(spark: SparkSession) -> None:
     print(df_raw.isStreaming)
     df_raw.printSchema()
 
+    # The key and value [both binary] received by the kafka topic are converted to string.
     df_raw = df_raw.withColumn("key", df_raw["key"].cast(StringType()))\
                    .withColumn("value", df_raw["value"].cast(StringType()))
 
+    # Event data Schema
     schema = StructType([
         StructField("$schema", StringType(), True),
         StructField("bot", BooleanType(), True),
@@ -61,41 +69,54 @@ def readKafkaStream(spark: SparkSession) -> None:
         StructField("wiki", StringType(), True)
     ])
 
+    # Create DataFrame setting schema for event data
     df_wikiStream = df_raw.withColumn("value", from_json("value", schema))
+    return df_wikiStream
 
-    df_wikiStream_formatted = df_wikiStream.select(
-                                                col("value.$schema").alias("schema"),
-                                                "value.bot",
-                                                "value.comment",
-                                                "value.id",
-                                                col("value.length.new").alias("length_new"),
-                                                col("value.length.old").alias("length_old"),
-                                                "value.minor",
-                                                "value.namespace",
-                                                "value.parsedcomment",
-                                                "value.patrolled",
-                                                col("value.revision.new").alias("revesion_new"),
-                                                col("value.revision.old").alias("revesion_old"),
-                                                "value.server_name",
-                                                "value.server_script_path",
-                                                "value.server_url",
-                                                col("value.timestamp").alias("change_timestamp"),
-                                                col("value.timestamp").alias("change_date"),
-                                                "value.title",
-                                                "value.type",
-                                                "value.user",
-                                                "value.wiki",
-                                                col("value.meta.domain").alias("meta_domain"),
-                                                col("value.meta.dt").alias("meta_dt"),
-                                                col("value.meta.id").alias("meta_id"),
-                                                col("value.meta.offset").alias("meta_offset"),
-                                                col("value.meta.partition").alias("meta_partition"),
-                                                col("value.meta.request_id").alias("meta_request_id"),
-                                                col("value.meta.stream").alias("meta_stream"),
-                                                col("value.meta.topic").alias("meta_topic"),
-                                                col("value.meta.uri").alias("meta_uri")
-                                                )
-    df_wikiStream_formatted.writeStream\
+def performs_some_transformation(df: DataFrame) -> DataFrame:
+    """Some transformation like, column renaming, and column conversion 
+    unix timestamp to timestamp, 
+    """
+    df_wikiStream_formatted = df.select(
+                                        col("value.$schema").alias("schema"),
+                                        "value.bot",
+                                        "value.comment",
+                                        "value.id",
+                                        col("value.length.new").alias("length_new"),
+                                        col("value.length.old").alias("length_old"),
+                                        "value.minor",
+                                        "value.namespace",
+                                        "value.parsedcomment",
+                                        "value.patrolled",
+                                        col("value.revision.new").alias("revesion_new"),
+                                        col("value.revision.old").alias("revesion_old"),
+                                        "value.server_name",
+                                        "value.server_script_path",
+                                        "value.server_url",
+                                        to_timestamp(from_unixtime(col("value.timestamp"))).alias("change_timestamp"),
+                                        to_date(from_unixtime(col("value.timestamp"))).alias("change_timestamp_date"),
+                                        "value.title",
+                                        "value.type",
+                                        "value.user",
+                                        "value.wiki",
+                                        col("value.meta.domain").alias("meta_domain"),
+                                        col("value.meta.dt").alias("meta_dt"),
+                                        col("value.meta.id").alias("meta_id"),
+                                        col("value.meta.offset").alias("meta_offset"),
+                                        col("value.meta.partition").alias("meta_partition"),
+                                        col("value.meta.request_id").alias("meta_request_id"),
+                                        col("value.meta.stream").alias("meta_stream"),
+                                        col("value.meta.topic").alias("meta_topic"),
+                                        col("value.meta.uri").alias("meta_uri")
+                                    )
+                                    
+    return df_wikiStream_formatted
+
+def main(spark: SparkSession) -> None:
+
+    data_raw = read_kafka_stream(spark)
+    data_formatted = performs_some_transformation(data_raw)
+    data_formatted.writeStream\
                         .format("console")\
                         .outputMode("append")\
                         .option("checkpointLocation", "/opt/bitnami/spark/tmp/checkpoint")\
@@ -114,6 +135,6 @@ if __name__=="__main__":
 
     spark.sparkContext.setLogLevel("WARN")
 
-    readKafkaStream(spark=spark)
+    main(spark)
 
     spark.stop()
